39709af9b2d001cf3a29f1bdb897228e04fe877b
unknown <isaiaspadros2@gmail.com>
2023-07-18 17:38:19 -0300

    Se completo el punto 1 del TP, falta README

---
+++ TpReact/src/components/Titulo.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Titulo = () => {
+    return (
+        <>
+        
+        <h1> Hello World !</h1>
+        
+        
+        </>
+    );
+};
+
+export default Titulo;
